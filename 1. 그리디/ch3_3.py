39ac1db77a110095bb533091211ec25c51bec457
Yewonhihi <81154552+yewonhihi@users.noreply.github.com>
2022-09-27 23:34:19 +0900

    9/27 그리디

---
+++ 1. 그리디/ch3_3.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    num = list(map(int, input().split()))
+    minValue = min(num)
+    if minValue > result:
+        result = minValue
+    
+print(result)
